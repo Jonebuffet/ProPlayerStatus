7dc41b30e758f98335a1494840f4da5079d615e9
jonebuffet <jonebuffet@gmail.com>
2020-02-01 14:01:00 -0800

    Adding file for working with any players yearly batting statistics

---
+++ src/com/wbbaseballstats/yearlystats/PlayerYearlyBattingStats.java
@@ -0,0 +1,167 @@
+package com.wbbaseballstats.yearlystats;
+
+public class PlayerYearlyBattingStats {
+	
+	private String playerid;
+	private int yearid;
+	private int ab;
+	private int bb;
+	private int cs;
+	private int doubles;
+	private int games;
+	private int gidp;
+	private int hbp;
+	private int hits;
+	private int hr;
+	private double batting_avg;
+	private int ibb;
+	private String lgid;
+	private int rbi;
+	private int runs;
+	private int sb;
+	private int sf;
+	private int sh;
+	private int so;
+	private int stint;
+	private String teamid;
+	private int triples;
+    
+	public String getPlayerid() {
+		return playerid;
+	}
+	public void setPlayerid(String playerid) {
+		this.playerid = playerid;
+	}
+	public int getYearid() {
+		return yearid;
+	}
+	public void setYearid(int yearid) {
+		this.yearid = yearid;
+	}
+	public int getAb() {
+		return ab;
+	}
+	public void setAb(int ab) {
+		this.ab = ab;
+	}
+	public int getBb() {
+		return bb;
+	}
+	public void setBb(int bb) {
+		this.bb = bb;
+	}
+	public int getCs() {
+		return cs;
+	}
+	public void setCs(int cs) {
+		this.cs = cs;
+	}
+	public int getDoubles() {
+		return doubles;
+	}
+	public void setDoubles(int doubles) {
+		this.doubles = doubles;
+	}
+	public int getGames() {
+		return games;
+	}
+	public void setGames(int games) {
+		this.games = games;
+	}
+	public int getGidp() {
+		return gidp;
+	}
+	public void setGidp(int gidp) {
+		this.gidp = gidp;
+	}
+	public int getHbp() {
+		return hbp;
+	}
+	public void setHbp(int hbp) {
+		this.hbp = hbp;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	public int getHr() {
+		return hr;
+	}
+	public void setHr(int hr) {
+		this.hr = hr;
+	}
+	public double getBatting_avg() {
+		return batting_avg;
+	}
+	public void setBatting_avg(double batting_avg) {
+		this.batting_avg = batting_avg;
+	}
+	public int getIbb() {
+		return ibb;
+	}
+	public void setIbb(int ibb) {
+		this.ibb = ibb;
+	}
+	public String getLgid() {
+		return lgid;
+	}
+	public void setLgid(String lgid) {
+		this.lgid = lgid;
+	}
+	public int getRbi() {
+		return rbi;
+	}
+	public void setRbi(int rbi) {
+		this.rbi = rbi;
+	}
+	public int getRuns() {
+		return runs;
+	}
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+	public int getSb() {
+		return sb;
+	}
+	public void setSb(int sb) {
+		this.sb = sb;
+	}
+	public int getSf() {
+		return sf;
+	}
+	public void setSf(int sf) {
+		this.sf = sf;
+	}
+	public int getSh() {
+		return sh;
+	}
+	public void setSh(int sh) {
+		this.sh = sh;
+	}
+	public int getSo() {
+		return so;
+	}
+	public void setSo(int so) {
+		this.so = so;
+	}
+	public int getStint() {
+		return stint;
+	}
+	public void setStint(int stint) {
+		this.stint = stint;
+	}
+	public String getTeamid() {
+		return teamid;
+	}
+	public void setTeamid(String teamid) {
+		this.teamid = teamid;
+	}
+	public int getTriples() {
+		return triples;
+	}
+	public void setTriples(int triples) {
+		this.triples = triples;
+	}
+}
